0e69b7b2e4878f2dbebe2309056c14d9f41ed88d
leslie <leslie43@sina.com>
2021-08-06 13:40:02 +0800

    @Emit is same as this.

--- src/views/Requisition/components/rqz-edit.vue
+++ src/views/Requisition/components/rqz-edit.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <rqz-type />
+    <rqz-man @man-change="handleManChange" />
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import RqzType from './rqz-type.vue'
-@Component({ components: { RqzType } })
-export default class RequisitionEdit extends Vue {}
+import RqzMan from './rqz-man.vue'
+@Component({ components: { RqzType, RqzMan } })
+export default class RequisitionEdit extends Vue {
+  handleManChange(man: string): void {
+    console.log(
+      '🚀 ~ file: rqz-edit.vue ~ line 14 ~ RequisitionEdit ~ handleManChange ~ man',
+      man
+    )
+  }
+}
 </script>
---
+++ src/views/Requisition/components/rqz-man.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-select
+      v-model="man"
+      :items="items"
+      filled
+      label="Filled style"
+      @change="handleChange"
+    ></v-select>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Emit, Vue } from 'vue-property-decorator'
+@Component({})
+export default class RequisitionMan extends Vue {
+  items = ['Foo', 'Bar', 'Fizz', 'Buzz']
+  man = ''
+  @Emit('man-change')
+  handleChange(): string {
+    return this.man
+  }
+}
+</script>
