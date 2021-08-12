f66225513067c447ddf920e83890da1eb68860f2
leslie <leslie43@sina.com>
2021-08-12 11:15:49 +0800

    tree view detail

--- src/views/TreeViewDetail/index.vue
+++ src/views/TreeViewDetail/index.vue
@@ -84,14 +84,8 @@
 
 <script lang="ts">
 import { Vue, Component, Watch } from 'vue-property-decorator'
-const avatars = [
-  '?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban',
-  '?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun',
-  '?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong',
-  '?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair',
-  '?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly',
-]
-const pause = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms))
+import avatars from './avatars/index'
+import { pause } from './util/pause'
 @Component({ name: 'TreeViewHome' })
 export default class TreeViewHome extends Vue {
   active = []
@@ -101,10 +95,7 @@ export default class TreeViewHome extends Vue {
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   async fetchUsers(item: any) {
-    // Remove in 6 months and say
-    // you've made optimizations! :)
     await pause(500)
-
     return fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((json) => item.children.push(...json))
