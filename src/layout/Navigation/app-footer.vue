0c86bff71f6bbf40a87b356d71dcc61e852d290c
Leslie Su <leslie43@sina.com>
2021-09-09 21:48:20 +0800

    vue ref get child inject prop

--- src/layout/Navigation/app-footer.vue
+++ src/layout/Navigation/app-footer.vue
@@ -5,11 +5,12 @@
 </template>
     
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop, Inject } from 'vue-property-decorator'
 import { LayoutStyleType } from '@/types/layout.type'
 
 @Component({})
 export default class AppFooter extends Vue {
   @Prop() layoutStyle!: LayoutStyleType
+  @Inject() readonly headerColor!: string
 }
 </script>
