cdf2e8da57593c40096fc08fe320adfea50ec4ef
Leslie Su <leslie43@sina.com>
2021-09-11 15:58:53 +0800

    mock return value once scenior

---
+++ src/decorators/class-decorator.ts
@@ -0,0 +1,16 @@
+function Path(path: string) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (target: Function) {
+    !target.prototype.$Meta && (target.prototype.$Meta = {})
+    target.prototype.$Meta.baseUrl = path
+  }
+}
+
+@Path('/hello')
+class HelloService {
+  constructor() {}
+}
+
+console.info(HelloService.prototype.$Meta)
+const hello = new HelloService()
+console.info(hello.$Meta)
--- tests/unit/mocks/jest.spy-on-depen-on-method.spec.ts
+++ tests/unit/mocks/jest.spy-on-depen-on-method.spec.ts
@@ -1,8 +1,13 @@
 class NumberMsg {
   public msg = ''
+  public cur = 0
 
   getRandom(): number {
     return Math.random() * 100
+  }
+
+  getFixValue(): number {
+    return 50
   }
 
   printResult(): void {
@@ -13,20 +18,38 @@ class NumberMsg {
       this.msg = 'Cool'
     }
   }
+
+  printFixResult(): void {
+    const num = this.getFixValue()
+    this.cur = num
+  }
 }
 
 describe('Name of the group', () => {
   const nm = new NumberMsg()
 
-  it('should return OK ', () => {
+  it('Test ## 1: should return OK ', () => {
     jest.spyOn(nm, 'getRandom').mockReturnValue(60)
     nm.printResult()
     expect(nm.msg).toBe('Good')
   })
 
-  it('should return OK ', () => {
+  it('Test ## 2: should return Cool ', () => {
     jest.spyOn(nm, 'getRandom').mockReturnValue(12)
     nm.printResult()
     expect(nm.msg).toBe('Cool')
+  })
+
+  it('Test ## 3: should return mock value ', () => {
+    // will impact test case 4, if not use mockReturnValueOnce
+    // you can try to change it to mockReturnValue to check the impact.
+    jest.spyOn(nm, 'getFixValue').mockReturnValueOnce(1)
+    nm.printFixResult()
+    expect(nm.cur).toBe(1)
+  })
+
+  it('Test ## 4: should return fix value ', () => {
+    nm.printFixResult()
+    expect(nm.cur).toBe(50)
   })
 })
