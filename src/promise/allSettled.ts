e64c54b3584afdb2020e6ee835dfcb6a210c9458
leslie <leslie43@sina.com>
2021-09-13 22:40:01 +0800

    promise.allSettled

--- src/promise/allSettled.ts
+++ src/promise/allSettled.ts
@@ -4,30 +4,44 @@ interface ItemResult {
   isGood: boolean
 }
 
-function makeItem({ counter }: { counter: number }): Promise<ItemResult> {
+function makeItemResolve({ counter }: { counter: number }): Promise<ItemResult> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (Math.random() > 0.5) {
-        resolve({ index: counter, id: Math.random(), isGood: true })
-      } else {
-        reject({ index: counter, id: Math.random(), isGood: false })
-      }
+      //   if (Math.random() > 0.5) {
+      resolve({ index: counter, id: Math.random(), isGood: true })
+      //   } else {
+      //     reject({ index: counter, id: Math.random(), isGood: true })
+      //   }
+    }, 1000)
+  })
+}
+function makeItemReject({ counter }: { counter: number }): Promise<ItemResult> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //   if (Math.random() > 0.5) {
+      // resolve({ index: counter, id: Math.random(), isGood: true })
+      //   } else {
+      reject({ index: counter, id: Math.random(), isGood: true })
+      //   }
     }, 1000)
   })
 }
 
 export const simulateSettled = async () => {
   // Need result in each async call => allSettled
-  const res = await Promise.allSettled([makeItem({ counter: 100 }), makeItem({ counter: 200 })])
+  const res = await Promise.allSettled([
+    makeItemResolve({ counter: 100 }),
+    makeItemReject({ counter: 200 }),
+  ])
   console.log('🚀 ~ file: allSettled.ts ~ line 21 ~ simulateSettled ~ res', res)
   /**
    * [
    *    {
-   *        reason:{id: 0.31266898661538356,index: 100, isGood: false},
+   *        reason: { id: 0.31266898661538356,index: 100, isGood: false},
             status: "rejected"
    *    },
         {
-   *        value:{id: 0.532172924038818, index: 200, isGood: true},
+   *        value: { id: 0.532172924038818, index: 200, isGood: true},
             status: "fulfilled"
    *    }
    * ]
