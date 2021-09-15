70b87a9712469a3f29c5bcf4648e1a8e99ab944a
leslie <leslie43@sina.com>
2021-09-15 08:47:40 +0800

    PropSync

--- src/components/CardSummary.vue
+++ src/components/CardSummary.vue
@@ -1,46 +1,51 @@
 <template>
-  <div class="summary-card">
-    <div class="summary-card-header-wrapper">
-      <div class="summary-card-header-title">{{ title }}</div>
-      <router-link :to="route">
-        <v-btn outlined small color="indigo">Go to Page ></v-btn>
-      </router-link>
+  <div>
+    <div>@PropSync('isShow', { type: Boolean, required: true }) isShowSync!: boolean</div>
+    <div>isShowSync:{{ isShowSync }}</div>
+    <div>isShow:{{ isShow }}</div>
+    <div class="summary-card" v-if="isShowSync">
+      <div class="summary-card-header-wrapper">
+        <div class="summary-card-header-title">{{ title }}</div>
+        <router-link :to="route">
+          <v-btn outlined small color="indigo">Go to Page ></v-btn>
+        </router-link>
+      </div>
+      <slot></slot>
+
+      <v-card class="mx-auto" max-width="400">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+          <v-card-title>Top 10 Australian beaches</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+        <v-card-text class="text--primary">
+          <div>Whitehaven Beach</div>
+
+          <div>Whitsunday Island, Whitsunday Islands</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="orange" text> Share </v-btn>
+
+          <v-btn color="orange" text> Explore </v-btn>
+        </v-card-actions>
+      </v-card>
     </div>
-    <slot></slot>
-
-    <v-card class="mx-auto" max-width="400">
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      >
-        <v-card-title>Top 10 Australian beaches</v-card-title>
-      </v-img>
-
-      <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
-
-      <v-card-text class="text--primary">
-        <div>Whitehaven Beach</div>
-
-        <div>Whitsunday Island, Whitsunday Islands</div>
-      </v-card-text>
-
-      <v-card-actions>
-        <v-btn color="orange" text> Share </v-btn>
-
-        <v-btn color="orange" text> Explore </v-btn>
-      </v-card-actions>
-    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component, Prop, PropSync } from 'vue-property-decorator'
 @Component({})
 export default class SummaryCardIndex extends Vue {
   @Prop({ required: true }) readonly title!: string
   @Prop({ required: true }) readonly route!: string
-
+  @PropSync('isShow', { type: Boolean, required: true }) isShowSync!: boolean
   name = ['su', 'zhen']
 }
 </script>
