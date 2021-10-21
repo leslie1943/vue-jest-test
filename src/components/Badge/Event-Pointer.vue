c5b3bc16f2324c88e3213236d896eae84a9b1324
leslie <leslie43@sina.com>
2021-10-21 22:43:50 +0800

    directive for click out of element

---
+++ src/components/Badge/Event-Pointer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="div_grandfather" onclick="alert('grandfather')">
+    <div class="div_parent" onclick="alert('parent')">
+      <div class="div_child" onclick="alert('child')"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class EventPointer extends Vue {}
+</script>
+
+<style >
+.div_grandfather {
+  height: 250px;
+  background: orange;
+  width: 300px;
+  border: 1px solid;
+  position: fixed;
+}
+.div_parent {
+  height: 70%;
+  background-color: #bbb;
+  margin: 30px;
+  border: 1px solid;
+  pointer-events: auto;
+}
+.div_child {
+  height: 70%;
+  background-color: deepskyblue;
+  margin: 30px;
+  pointer-events: none; /*  点击不会触发 */
+}
+</style>
