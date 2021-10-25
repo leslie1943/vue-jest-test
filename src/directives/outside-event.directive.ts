8d4ce6d218119d3f11d85ecb04834e977619a308
leslie <leslie43@sina.com>
2021-10-25 22:04:13 +0800

    add explanation for direcitve

--- src/directives/outside-event.directive.ts
+++ src/directives/outside-event.directive.ts
@@ -1,4 +1,7 @@
 export default {
+  // bind: 只调用一次,指令第一次绑定到元素时调用,用这个钩子函数可以定义一个在绑定时执行一次的初始化动作.
+  // bind: call once,  the first time when directive binding to element.
+  // can use this hook define init event handler
   bind: function (el: any, binding: any, vNode: any) {
     el._outsideEvent_ = (event: any) => {
       if (!el.contains(event.target)) {
@@ -9,6 +12,7 @@ export default {
     }
     document.body.addEventListener('click', el._outsideEvent_)
   },
+  // unbind: 只调用一次, 指令与元素解绑时调用.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   unbind: function (el: any, _binding: any, _vNode: any) {
     // Remove Event Listeners
