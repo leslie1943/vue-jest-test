efe4f3d0c911f10479f2d1ad4944db455efd3d31
leslie <leslie43@sina.com>
2021-11-12 00:21:16 +0800

    clearCookie

---
+++ src/views/Cypress/command/api-clear-cookie.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>DEMO for clear cookie</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class CypressCommandClearCookie extends Vue {}
+</script>
