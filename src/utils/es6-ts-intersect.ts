f86b13cdadedc2ec99556ccc4b6f4b56394448c5
leslie <leslie43@sina.com>
2021-11-24 11:06:13 +0800

    typescript 交叉类型

--- src/utils/es6-ts-intersect.ts
+++ src/utils/es6-ts-intersect.ts
@@ -8,12 +8,33 @@ interface IEmployee {
 }
 
 type IStaff = IWork & IEmployee
+type UStaff = IWork | IEmployee
 
-const staff: IStaff = {
+const istaff: IStaff = {
   id: 12,
   name: 'leslie',
   companyId: 2212,
 }
-console.log('🚀 ~ file: es6-ts-intersect.ts ~ line 17 ~ staff', staff)
+console.log('🚀 ~ file: es6-ts-intersect.ts ~ line 17 ~ staff', istaff)
+
+const ustaff_1: UStaff = {
+  companyId: 1234,
+  //   name: 'leslie',
+  //   id: 12,
+}
+const ustaff_2: UStaff = {
+  companyId: 1234,
+  //   name: 'leslie',
+  id: 12,
+}
+const ustaff_3: UStaff = {
+  //   companyId: 1234,
+  name: 'leslie',
+  id: 12,
+}
+// Union类型,必须<<<<至少>>>>满足一个类型
+console.log('🚀 ~ file: es6-ts-intersect.ts ~ line 25 ~ ustaff', ustaff_1)
+console.log('🚀 ~ file: es6-ts-intersect.ts ~ line 25 ~ ustaff', ustaff_2)
+console.log('🚀 ~ file: es6-ts-intersect.ts ~ line 25 ~ ustaff', ustaff_3)
 
 export {}
