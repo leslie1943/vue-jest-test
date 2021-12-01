a1e2aa52254f9d81be5e18c9fb2dc4b892b3e4ce
leslie <leslie43@sina.com>
2021-12-01 21:24:30 +0800

    cypress report

--- tests/e2e/cypress.mocha.js
+++ tests/e2e/cypress.mocha.js
@@ -6,7 +6,10 @@ const generator = require('mochawesome-report-generator')
 
 async function runTests() {
   await fse.remove('mochawesome-report')
-  const { totalFailed } = await cypress.run()
+  const { totalFailed } = await cypress.run({
+    configFile: './tests/e2e/cypress.mocha.report.json',
+    browser: 'edge',
+  })
   const jsonReport = await merge()
   await generator.create(jsonReport)
   process.exit(totalFailed)
