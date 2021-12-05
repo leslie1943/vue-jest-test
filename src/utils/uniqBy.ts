dff2677e94d5657a4537e0bbb622428029c842e1
leslie <leslie43@sina.com>
2021-12-05 18:25:08 +0800

    regex vertical

--- src/utils/uniqBy.ts
+++ src/utils/uniqBy.ts
@@ -1,15 +1,28 @@
-const uniqBy = (arr: any, predicate: any) => {
+/**
+ * @param arr
+ * @param predicate
+ */
+const uniqBy = (arr: Array<any>, predicate: string) => {
   const cb = typeof predicate === 'function' ? predicate : (o: any) => o[predicate]
   return [
     ...arr
       .reduce((map: any, item: any) => {
         const key = item === null || item === undefined ? item : cb(item)
-
         map.has(key) || map.set(key, item)
-
         return map
       }, new Map())
       .values(),
   ]
 }
+
+const arr = [
+  { name: 'leslie' },
+  { name: 'leslie' },
+  { name: 'moon' },
+  { name: 'moon' },
+  { name: 'micheal' },
+  { name: 'micheal' },
+]
+const res = uniqBy(arr, 'name')
+console.log('🚀 ~ file: uniqBy.ts ~ line 23 ~ res', res)
 export { uniqBy }
