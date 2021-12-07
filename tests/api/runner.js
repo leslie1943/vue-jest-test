08e9d099e7f9f79726a3a1c9b7ae09a9ae91d32e
leslie <leslie43@sina.com>
2021-12-08 00:03:24 +0800

    module.export export

---
+++ tests/api/runner.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+const { person } = require('./person')
+const personModule = require('./person')
+const breadModule = require('./bread')
+console.info(person)
+console.info(personModule)
+console.info(breadModule)
+console.info(breadModule.data)
